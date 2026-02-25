30cf6cb79e373f907337f3a04172eb043c4e2f39
builinht <builinhaqua@gmail.com>
2026-02-25 15:46:34 +0700

    sss

--- geo-backend/server.js
+++ geo-backend/server.js
@@ -118,7 +118,7 @@ async function startServer() {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
 
-    const PORT = process.env.PORT || 8080;
+    const PORT = process.env.PORT || 5000;
 
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on ${PORT}`);
