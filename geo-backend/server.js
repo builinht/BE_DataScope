194757532964141c1d76bd2e134baa2ab439bc67
builinht <builinhaqua@gmail.com>
2026-02-25 15:41:13 +0700

    fix port

--- geo-backend/server.js
+++ geo-backend/server.js
@@ -113,5 +113,8 @@ app.use((err, req, res, next) => {
 /* ======================
    7. START SERVER
 ====================== */
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+const PORT = process.env.PORT;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
